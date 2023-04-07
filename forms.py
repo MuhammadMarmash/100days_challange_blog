2fbb23a4ea3406deb9324571107b3635ec0396e2
MuhammadMarmash <hamada500014@gmail.com>
2023-04-07 15:22:32 +0300

    little

--- forms.py
+++ forms.py
@@ -21,7 +21,7 @@ class RegisterForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(), email()])
+    email = StringField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Sign Me Up!")
 
